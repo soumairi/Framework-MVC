174e6034918841dc56f18eb027e21237b90bc756
Soumairi Mouhssine <soumairi@gmail.com>
2017-03-02 14:41:19 +0000

    MVC Routing bootstarp

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");//le nom de serveur
+define("DB_USER", "root");//utilisateur 
+define("DB_PASS", "");//mot de passe
+define("DB_NAME", "nom_base");//nom de la base de donnees
+define("NBR_PAGES", 10);//les nombres des articles pages ;
+
+// Define URL
+define("ROOT_PATH", "/nom_dossier/");// definir le nom de dossier racine de site
+define("ROOT_URL", "http://localhost/nom_dossier/");// definir le chemin de site
---
+++ views/home/index.php
@@ -0,0 +1,5 @@
+<div class="text-center">
+	<h1>Welcome To My Framework</h1>
+	<p class="lead">SOUMAIRI Mouhssine</p>
+	
+</div>
